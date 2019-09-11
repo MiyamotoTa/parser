d20b2c33608e33a3d1a6b1f18a4a07d7481867de
Takahiro Miyamoto <takahiro.miyamoto@glv.co.jp>
2019-09-11 22:53:23 +0900

    Add Location

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+/// 位置情報。 .0から.1までの区間を表す
+/// たとえばLoc(4, 6) なら入力文字の5文字目から7文字目までの区間を表す(0始まり)
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Loc(usize, usize);
+
+// Locに便利メソッドを実装しておく
+impl Loc {
+    fn merge(&self, other: &Loc) -> Loc {
+        use std::cmp::{max, min};
+        Loc(min(self.0, other.0), max(self.1, other.1))
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
